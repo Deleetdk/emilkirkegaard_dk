08a3f99bf5073cbe55da5262af4bde7211d40b6d
Emil O. W. Kirkegaard <emil@emilkirkegaard.dk>
2015-10-01 14:07:18 -0500

    Initial release

---
+++ index.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Emil O. W. Kirkegaard - Personal site</title>
+
+    <!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Template CSS -->
+    <link href="css/freelancer.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/custom.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- jquery for tablesorter -->
+    <script src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/jquery.tablesorter.js"></script>
+
+</head>
+
+<body id="page-top" class="index">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#page-top">Top</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#about">About/contact</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#interests">Interests</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#publications">Publications</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#projects">Projects</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <!-- Header -->
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <img class="img-responsive" id="mainPhoto" src="img/profile.png" alt="">
+                    <div class="intro-text">
+                        <span class="name">Emil Ole William Kirkegaard</span>
+                        <span class="skills">Polymath scientist, maybe</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- About Section -->
+    <section class="success" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>About/contact</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 special_links">
+                    <p>I am a 26 year old polymath wannabe(?), see very long list of interests <a href="#interests">below</a>. I am mostly self-taught in various subjects and currently a master's student in linguistics at Aarhus University, Denmark (<a href="http://lyddansk.dk/?p=226">my bachelor thesis</a> concerned spelling reform with a focus on a proposal for Danish I developed). Basically, I do it mostly for the money (<a href="https://en.wikipedia.org/wiki/Student_loans_in_Denmark">the Danish state will pay you to study AND education is free</a>) and spend my free time studying whatever interests me.</p>
+                    <p>I write lots of stuff on <a href="/en">my English language blog</a>, although I also have <a href="/da">a Danish one</a> and even <a href="/eo">an inactive Esperanto one</a>. My more formal academic publications can be found <a href="#publications">further down this page</a> but lots of more original data analyses can be found on my blogs.</p>
+                    <p>Along with <a href="https://www.researchgate.net/profile/Davide_Piffer">Davide Piffer</a> I started an <a href="https://en.wikipedia.org/wiki/Open_science">open science publisher</a>, <a href="http://openpsych.net/">OpenPsych</a>, which offers free to publish, <a href="http://openpsych.net/forum/showthread.php?tid=5">open peer review</a>, open access, open data etc. The journals mostly publish stuff related to individual and group differences in cognitive ability, personality and the associated socioeconomic outcomes.</p>
+                    <p>I have yet to find any major political ism that is in line with my beliefs, but <a href="http://raikoth.net/libertarian.html">Scott Alexander's essay against libertarianism</a> comes pretty close. Briefly put, the main political ideologies were invented before we had much of a clue how the world worked and so it would a miracle if any one of them got it just right. I favor scientific rationalism and apply the same approach to politics. My meta-politics stance is consequentialism. I favor whatever has the best outcomes no matter which ideology it is in line with. I have been active in the Danish <a href="https://en.wikipedia.org/wiki/Pirate_Party">Pirate Party</a> in relation to internet freedoms, civil liberties (real ones, not those pushed by social justice warriors) and <a href="https://www.goodreads.com/book/show/3770306-against-intellectual-monopoly">harmful intellectual monopolies</a>.</p>
+                    <p>I can be found many places, but am especially active on <a href="https://twitter.com/KirkegaardEmil">Twitter</a> with science related things. I can also be found on <a href="http://www.goodreads.com/user/show/8884040-emil-kirkegaard">Goodreads</a>, <a href="https://www.researchgate.net/profile/Emil_O_W_Kirkegaard">ResearchGate</a>, <a href="http://scholar.google.dk/citations?user=VKUbfSIAAAAJ&hl=en">Google Scholar</a>, <a href="https://github.com/Deleetdk/">GitHub</a>, <a href="https://osf.io/a2yfn/">Open Science Framework</a>, <a href="https://www.facebook.com/emilkirkegaard">Facebook</a>, <a href="https://plus.google.com/u/0/105966274782785141105/posts">Google+</a>, <a href="https://www.linkedin.com/profile/view?id=279385313">LinkedI</a>, <a href="http://lastfm.com/user/deleetdk">LastFM</a>, and probably some other places.</p>
+                    <p>You can contact me (my first name)@emilkirkegaard.(<a href="https://en.wikipedia.org/wiki/Country_code_top-level_domain">Danish TLD</a>) or on <a href="https://en.wikipedia.org/wiki/Tox_%28protocol%29">Tox</a> with <a href="1728E9D22CDDDDBE314E002843E7F57A20365D40CF0F6B26803AD68A163E82710C78A5213A9B">this ID</a>. If you don't know which client to use, try <a href="https://github.com/tux3/qTox">qTox</a>.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    
+    <!-- Interests Grid Section -->
+    <section id="interests" class="alt">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Interests</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                        <p>My interests are very broad, but for the time I am mainly focused on psychology, sociology, statistics and  programming. A non-exhaustive list of areas of interest can be seen below. I have somewhat ranked them by current interest level.</p>
+                </div>
+                <div class="col-lg-8 col-lg-offset-2 text-left">
+                    <ul>
+                        <li>Psychology</li>
+                        <ul>
+                            <li>Cognitive abilities</li>
+                            <li>Personality</li>
+                            <li>Behavior genetics</li>
+                            <li>Evolutionary psychology</li>
+                            <li>Nootropics</li>
+                        </ul>
+                        <li>Programming/computer science</li>
+                        <ul>
+                            <li>R</li>
+                            <li>Automatic mixed explorative/confirmatory methods</li>
+                            <li>Machine learning</li>
+                            <li>Data scraping</li>
+                            <li>Python</li>
+                        </ul>
+                        <li>Open science</li>
+                        <li>Political theory</li>
+                        <li>Linguistics</li>
+                        <ul>
+                            <li>Spelling systems (focus on Danish)</li>
+                            <li>Language planning/reform</li>
+                        </ul>
+                        <li>Game studies/ludology</li>
+                        <li>Evidence-based policies</li>
+                        <ul>
+                            <li>Immigration</li>
+                            <li>Civil rights</li>
+                            <li>Drug policy</li>
+                            <li>Intellectual 'property'/monopolies (copyright, patents, trademarks)</li>
+                            <li>Seasteading</li>
+                        </ul>
+                        <li>Philosophy</li>
+                        <ul>
+                            <li>Philosophy of science</li>
+                            <li>Formal and informal logic</li>
+                            <li>Philosophy of language</li>
+                            <li>Metaethics and ethics</li>
+                            <li>Transhumanism</li>
+                            <li>Metaphysics of logic</li>
+                        </ul>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Projects section -->
+    <section class="success" id="projects">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Projects</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <p>Due to my diverse interests I have many on-going projects in various stages of completation and seriousness. The list below mentions some of the larger ones.</p>
+                    <ul class="no_points">
+                        <li>
+                            <h3><a href="http://openpsych.net/">OpenPsych</a></h3>
+                            <p>An open acccess, free-to-publish, data-sharing, open peer review metajournal right now focusing on differential psychology and behavioral genetics but we will expand.</p>
+                        </li>
+                        <li>
+                            <h3>The general socioeconomic factor (S factor)</h3>
+                            <p>An on-going research project into the general factor of socioeconomic inequality at various levels of analysis: countries and <a href="https://en.wikipedia.org/wiki/Table_of_administrative_divisions_by_country">various administrative divisions within countries</a>, immigrants classified by country of origin, persons classified by first or last names, and so on. The most pressing concern is writing a review article summarizing the many studies I have carried out so far.</p>
+                        </li>
+                        <li>
+                            <h3>Spatial transferability hypothesis</h3>
+                            <p>An on-going research project in collaboration with John Fuerst into the psychological traits of immigrants and socioeconomic outcomes in relationship to their countries of origin. So far research strongly confirms that immigrant performance is predicted by country of origin variables. The most pressing concern is writing a review article of the already large body of research.</p>
+                        </li>
+                        <li>
+                            <h3>The OKCupid project</h3>
+                            <p>OKCupid contains thousands of user-generated questions used for matching persons. Since these answers are public they can be gathered automatically. Since late 2014 we have been scraping the data form OKCupid and currently have a database of some 70,000 unique users and their answers to approximately 2000 questions as well as demogaphic informaiton. Currently unpublished.</p>
+                        </li>
+                        <li>
+                            <h3><a href="http://lyddansk.dk">Lyddansk</a></h3><br>
+                            <p>A website arguing the benefits of spelling reform with a focus on Danish (in Danish).</p>
+                        </li>
+                        <li>
+                            <h3><a href="http://legaliser.nu">Legaliser.nu</a></h3><br>
+                            <p>A website arguing the benefits of legalization of psychoactive drugs (in Danish).</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- Publications Section -->
+    <section class="alt" id="publications">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Publications</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <p>Since there are high time-costs, and sometimes monetary as well, associated with publishing in pre-publication review journals and in many cases <a href="https://en.wikipedia.org/wiki/Scholarly_peer_review#Criticism_of_peer_review">a relatively small benefit</a> aside from reputational gain, I often publish in outlets with no prior review.</p>
+                    <p>The list below contains my more formal publications and is sortable in various ways. To be notified in case of new publications, follow me on <a href="https://www.researchgate.net/profile/Emil_O_W_Kirkegaard">ResearchGate</a> or  <a href="http://scholar.google.dk/citations?user=VKUbfSIAAAAJ&hl=en">Google Scholar</a></p>
+                </div>
+                <div class="col-lg-12 text-left">
+                <?php
+                $result = array_map('str_getcsv', file('publications.csv'));
+                #make table
+                echo "<table id='publicationsTable' class='tablesorter'>\n"; // start a table tag in the HTML
+                echo "<thead><tr><th class='sortableHeader'>Title</th>
+                                 <th class='sortableHeader'>Author(s)</th>
+                                 <th class='sortableHeader'>Type</th>
+                                 <th class='sortableHeader'>Outlet</th>
+                                 <th class='sortableHeader'>Pre-publication review</th>
+                                 <th class='sortableHeader'>Date</th>
+                                 <th class='sortableHeader'>Sole author</th>
+                                 <th class='sortableHeader'>First author</th>
+                                 <th class='sortableHeader'>Last author</th></tr></thead>\n";
+                echo "<tbody>\n";
+                foreach ($result as $row_idx => $row) {
+                    if ($row_idx == 0) {
+                        continue;
+                    }
+                    echo "<tr>
+                    <td><a href='" . $row[10] . "'>" . $row[3] . "</a></td>
+                    <td>" . $row[1] . "</td>
+                    <td>" . $row[2] . "</td>
+                    <td>" . $row[4] . "</td>
+                    <td>" . $row[5] . "</td>
+                    <td>" . $row[6] . "</td>
+                    <td>" . $row[7] . "</td>
+                    <td>" . $row[8] . "</td>
+                    <td>" . $row[9] . "</td>
+                    </tr>\n";
+                }
+                echo "</tbody>\n";
+                echo "</table>\n"; //Close the table in HTML
+
+                ?>
+                <script type="text/javascript">
+                $(document).ready(function()
+                    {
+                        $("#publicationsTable").tablesorter(  {sortList: [[5,1]]}  );
+                    }
+                );
+                </script>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    
+
+    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+    <div class="scroll-top page-scroll visible-xs visible-sm">
+        <a class="btn btn-primary" href="#page-top">
+            <i class="fa fa-chevron-up"></i>
+        </a>
+    </div>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/cbpAnimatedHeader.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/freelancer.js"></script>
+
+</body>
+
+</html>
